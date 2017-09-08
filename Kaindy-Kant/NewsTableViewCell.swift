6a69502a2f55fced5bbcdc17ee3401f4f542f3bb
Begimai <Begimai.amantaeva@gmail.com>
2017-09-08 10:35:38 +0600

    mainPageViewController

---
+++ Kaindy-Kant/NewsTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  NewsTableViewCell.swift
+//  Kaindy-Kant
+//
+//  Created by Niko on 9/8/17.
+//  Copyright © 2017 NeoBis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var newsTitleLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
