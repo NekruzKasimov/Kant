f56a6326bee5715b12a3f983d0fa71f43feb2779
ZYFAR <zyfar@ITLabAdmins-iMac-2.local>
2017-10-04 15:14:11 +0600

    add NewsVC in SlideMenu

--- Kaindy-Kant/NewsTableViewCell.swift
+++ Kaindy-Kant/NewsTableViewCell.swift
@@ -19,6 +19,7 @@ class NewsTableViewCell: UITableViewCell {
             newsView.layer.shadowOpacity = 0.8
         }
     }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
---
+++ Kaindy-Kant/NewsViewController.swift
@@ -0,0 +1,69 @@
+//
+//  NewsViewController.swift
+//  Kaindy-Kant
+//
+//  Created by ZYFAR on 03.10.17.
+//  Copyright © 2017 NeoBis. All rights reserved.
+//
+
+import UIKit
+
+enum NewsSections: Int {
+    case sugar = 0
+    case news  = 1
+    
+    func getItemsCount() -> Int {
+        switch self {
+        case .sugar:
+            return 1
+        case .news:
+            return 4
+        }
+    }
+}
+
+class NewsViewController: UIViewController {
+
+    @IBOutlet weak var newsTableView: UITableView! {
+        didSet {
+            newsTableView.register(NewsTableViewCell.self, forCellReuseIdentifier: "NewsTableViewCell")
+            newsTableView.register(UINib(nibName: "NewsTableViewCell", bundle: nil), forCellReuseIdentifier: "NewsTableViewCell")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavigationBar()
+    
+    }
+}
+
+extension NewsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return NewsSections(rawValue: section)!.getItemsCount()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        
+        switch NewsSections(rawValue: indexPath.section)! {
+        case .sugar:
+            cell = tableView.dequeueReusableCell(withIdentifier: "SugarTableViewCell")!
+            break
+        case .news:
+            cell = tableView.dequeueReusableCell(withIdentifier: "NewsTableViewCell") as! NewsTableViewCell
+            break
+        }
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 204
+    }
+}
