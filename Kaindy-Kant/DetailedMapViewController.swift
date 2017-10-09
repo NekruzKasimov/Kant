9371dd323c28b4742e21ae774598374ef6234d8e
ZYFAR <zyfar@ITLabAdmins-iMac-2.local>
2017-10-09 17:51:53 +0600

    update DetailedMapVC

--- Kaindy-Kant/DetailedMapViewController.swift
+++ Kaindy-Kant/DetailedMapViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 import MapKit
 
-class DetailedMapViewController: UIViewController {
 
+class DetailedMapViewController: UIViewController {
+    
+    @IBOutlet weak var detailedMap: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        addMap(latitude: 42.81064, longitude: 74.627359)
+    }
+    
+    func addMap(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: location, span: span)
+        detailedMap.setRegion(region, animated: true)
     }
 }
