818ae5788d66f60342d12e711ea3b8818efc749c
Nekruz <kasimov.nek@gmail.com>
2017-11-01 19:46:25 +0600

    UI Update Calculator, collectionViewCell updated

--- Kaindy-Kant/FirstSectionTableViewCell.swift
+++ Kaindy-Kant/FirstSectionTableViewCell.swift
@@ -10,40 +10,12 @@ import Foundation
 
 class FirstSectionTableViewCell: UITableViewCell {
     
+    @IBOutlet weak var typesOfJobLabel: UILabel!
     
-    @IBOutlet weak var firstLabel: UILabel! {
-        didSet {
-            firstLabel.layer.borderWidth = 1
-            firstLabel.backgroundColor = UIColor.init(netHex: Colors.gray)
-        }
-    }
+    @IBOutlet weak var priceLabel: UILabel!
     
-    @IBOutlet weak var typesOfJobLabel: UILabel! {
-        didSet {
-            typesOfJobLabel.layer.borderWidth = 1
-            typesOfJobLabel.backgroundColor = UIColor.init(netHex: Colors.gray)
-        }
-    }
+    @IBOutlet weak var amountLabel: UILabel!
     
-    @IBOutlet weak var priceLabel: UILabel! {
-        didSet {
-            priceLabel.layer.borderWidth = 1
-            priceLabel.backgroundColor = UIColor.init(netHex: Colors.gray)
-        }
-    }
-    
-    @IBOutlet weak var amountLabel: UILabel! {
-        didSet {
-            amountLabel.layer.borderWidth = 1
-            amountLabel.backgroundColor = UIColor.init(netHex: Colors.gray)
-        }
-    }
-    
-    @IBOutlet weak var totalPriceLabel: UILabel! {
-        didSet {
-            totalPriceLabel.layer.borderWidth = 1
-            totalPriceLabel.backgroundColor = UIColor.init(netHex: Colors.gray)
-        }
-    }
+    @IBOutlet weak var totalPriceLabel: UILabel! 
     
 }
